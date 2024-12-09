aef1dab350550c0b84e5485033baba0fbab73439
GeeTechLabs <geetechlabs@gmail.com>
2024-12-09 19:28:17 +0300

    Arithmetic correction

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -3,7 +3,7 @@ number2 = 5
 
 addition = number1 + number2
 difference = number1 - number2
-product = number1 - number2
+product = number1 * number2
 
 print(f'Addition of {number1} and {number2} is {addition}')
 print(f'Subtraction of {number1} and {number2} is {difference}')
