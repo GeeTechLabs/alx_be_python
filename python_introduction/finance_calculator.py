66502b735dd2cbeddc33cdffc477343078c7811e
GeeTechLabs <geetechlabs@gmail.com>
2024-12-11 18:06:51 +0300

    Addition of Float in Finance script

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -2,7 +2,7 @@ monthly_income = int(input("Enter your monthly income: "))
 monthly_expense = int(input("Enter your total monthly expenses: "))
 
 
-monthly_savings = monthly_income - monthly_expense
+monthly_savings = (monthly_income - monthly_expense|float)
 
 projected_Savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)
 
